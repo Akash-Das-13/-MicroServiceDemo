f19a4c5796f907159e5160ac526f58da38671ac1
akash.das <akash.das@cgi.com>
2020-11-02 17:53:34 +0530

    Initial commit

---
+++ test1/test.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace test1
+{
+    public class test
+    {
+        [Fact]
+        public void Test1()
+        {
+            var flag = true;
+            Assert.True(flag);
+
+        }
+    }
+}
